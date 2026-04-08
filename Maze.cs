83a11ddcc7cbc6cee02b8482f602f6a4b9e1859f
mikeg <mgustafson1@zynex.com>
2026-04-07 20:05:54 -0500

    Bitmap load/save and conversion to Feature array implemented

---
+++ Maze.cs
@@ -0,0 +1,65 @@
+﻿
+namespace BluebeamMaze;
+
+/// <summary>
+/// This class represents a maze as a set of interconnected rooms. Each room
+/// points back to the original maze in terms of coordinates.
+/// </summary>
+internal class Maze
+{
+	public void Load(Feature[,] map)
+	{
+
+	}
+
+	#region Bitmap to Feature array conversion
+
+	/// <summary>
+	/// Converts an RGB bitmap into a more convenient 2D array of features
+	/// </summary>
+	/// <param name="bitmap">The raw pixel bitmap</param>
+	/// <returns>A two dimensional array of features</returns>
+	public static Feature[,] TranslateBitmap (Bitmap bitmap)
+	{
+		var features = new Feature[bitmap.Width, bitmap.Height];
+		for (int x = 0; x != bitmap.Width; x++)
+			for (int y = 0; y != bitmap.Height; y++)
+				features[x, y] = ConvertPixelToFeature(bitmap.GetPixel(x, y));
+		return features;
+	}
+
+	/// <summary>
+	/// Using fixed color values since the input maze is very specific, but this could
+	/// be expanded for other features and could be built dynamically by scanning colors
+	/// in ranges so we wouldn't be stuck with just pure colors.
+	/// </summary>
+	private static Feature ConvertPixelToFeature(Color color)
+	{
+		if (!_pixelFeatureMap.TryGetValue(color, out var feature))
+			throw new Exception("Maze contains an unsupported pixel color");
+
+		return feature;
+	}
+
+	private static Dictionary<Color, Feature> _pixelFeatureMap = new()
+	{
+		[Color.FromArgb(255, 0, 0, 255)] = Feature.Start,		// Blue
+		[Color.FromArgb(255, 255, 0, 0)] = Feature.End,			// Red
+		[Color.FromArgb(255, 255, 255, 255)] = Feature.None,	// White
+		[Color.FromArgb(255, 0, 0, 0)] = Feature.Wall,			// Black
+	};
+
+	#endregion
+}
+
+/// <summary>
+/// This is the translation from color to what maze feature it represents. This
+/// makes the code cleaner when determining the room connections.
+/// </summary>
+internal enum Feature
+{
+	None,
+	Wall,
+	Start,
+	End,
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,42 @@
-namespace BluebeamMaze
+
+namespace BluebeamMaze;
+
+internal static class Program
 {
-    internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
-        }
-    }
+	[STAThread]
+	static void Main(string[] args)
+	{
+		// give usage message if incorrect number of arguments
+		if (args.Length != 2)
+		{
+			Console.WriteLine("Usage: maze \"source.[bmp,png,jpg]\" \"destination.[bmp,png,jpg]\"");
+			return;
+		}
+
+		// this will standardize the source and destination filenames (such as remove quotes).
+		var sourceFile = Path.GetFullPath(args[0]);
+		var destFile = Path.GetFullPath(args[1]);
+
+		// check for existence of source file
+		if (!File.Exists(sourceFile))
+		{
+			Console.WriteLine($"Error: File \"{sourceFile}\" not found");
+			return;
+		}
+
+		try
+		{
+			var mazeBitmap = new Bitmap(sourceFile);
+			Console.WriteLine($"Maze size is {mazeBitmap.Width} x {mazeBitmap.Height}");
+
+			var features = Maze.TranslateBitmap(mazeBitmap);
+
+			Console.WriteLine($"Saving solved maze to \"{destFile}\"");
+			mazeBitmap.Save(destFile);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error: Maze failed with exception: {ex.Message}");
+		}
+	}
 }
